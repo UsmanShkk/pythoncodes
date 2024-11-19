fd70dcfdf97afe45f3b5f4a109f13debbb741f97
UsmanShkk <shkmusman455@gmail.com>
2024-11-19 22:37:50 +0500

    Add files via upload

---
+++ TWO RIVAL STUDENTS.py
@@ -0,0 +1,13 @@
+t=int(input())
+for z in range(t):
+    n,x,a,b=map(int,input().split())
+    if a<b:
+        diff1=abs(1-a)
+        diff2=abs(n-b)
+    else:
+        diff1 = abs(1 - b)
+        diff2 = abs(n - a)
+    if x>=(diff1+diff2):
+        print(diff1+diff2+abs(a-b))
+    else:
+        print(x+abs(a-b))
